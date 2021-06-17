81eaf1f7b80b9982fad44b18ee78de756997e5ff
Paul Gay <pagay@frontend-buster>
2021-06-17 17:58:58 +0200

    packaging the code

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ setup(name='deep_learning_mcmc',
       version='1.0',
       packages=['deep_learning_mcmc'],
       )
-"""
 install_requires=[
-       'psutil',
-       'pandas',
        'numpy',
-       'networkx',
-   ],
-"""
+   ]
